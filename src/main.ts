40e94da26cf624f6a90691ffe3d70b84ec061eb9
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2022-11-12 10:49:49 +0900

    サーバー情報を隠す　ポート番号の変更

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+// サーバー情報を隠す
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // サーバー情報を隠す
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // サーバー情報を隠す
+  app.disable('x-powered-by');
+  // ポート番号の変更はここでできる！
+  await app.listen(8000);
 }
 bootstrap();
